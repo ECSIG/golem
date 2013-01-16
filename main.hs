e60a8d5dc3b22a6e8e0558050add7506a37b6c53
Jasper Darjeeling <jasper@mimsy.us>
2013-01-16 15:22:50 -0500

    added datastructures; altered flow

--- main.hs
+++ main.hs
@@ -1,193 +1,160 @@
---------------------
--- GOLEM THE ROBOT --
---------------------
-
-import Network
-import System.IO
-import Text.Printf
-import Data.List
-import System.Exit
-import System.Environment (getArgs)
-import Control.Monad.Reader
-import Control.Exception
-import System.Time
+import Network.IRC
+import System.Exit(exitSuccess)
+import Control.Exception(bracket)
 import Control.Applicative((<$>))
-import Prelude hiding (catch)
-import Control.Concurrent (threadDelay)
-import qualified Network.IRC.Commands as IrcC
-import qualified Network.IRC.Base as IrcB
-import qualified Network.IRC.Parser as IrcP
- 
-name = "golem"
-chan = "#golem"
-serv = "irc.ecsig.com"
-port = 6667
+import Control.Monad(forever)
+import Control.Monad.Reader(local,ask,asks,ReaderT,runReaderT,liftIO)
+import Data.Char(isSpace)
+import Data.List(isPrefixOf)
+import Network(PortID(PortNumber),connectTo)
+import System.IO(Handle,hClose,hGetLine,hPutStrLn,hSetBuffering,BufferMode(NoBuffering))
 
-type Net = ReaderT Bot IO
+defaultName = "golbat"
+defaultChan = "#golem"
+defaultServ = "irc.ecsig.com"
+defaultPort = PortNumber 6667
+defaultActs = [("quit", quitAct),
+              ("echo", echoAct),
+              ("join", joinAct),
+              ("part", partAct),
+              ("nick", nickAct)]
 
-data Bot = Bot { startTime :: ClockTime
-               , socket :: Handle
-               , nickname :: IrcB.UserName
-               , channel :: IrcC.Channel
-               , server :: IrcB.ServerName
-               }
+data IrcData = IrcData
+    { handle   :: Handle
+    , userName :: UserName
+    , actions  :: ActionList
+    }
 
--- Set up actions to run on start and end, and run the main loop
-main :: IO ()
-main = bracket connect disconnect loop
-  where
-    disconnect = hClose . socket
-    loop       = runReaderT run
+type IrcState = ReaderT IrcData IO
 
--- builds a bot from defaults & provided args
-connect :: IO Bot
-connect = notify $ do
-  args <- getArgs
-  makeBot name chan serv args
-    where makeBot n c s (a:as)
-              | "nick=" `isPrefixOf` a
-                  = makeBot (drop 5 a) c s as
-              | "channel=" `isPrefixOf` a
-                  = makeBot n (drop 8 a) s as
-              | "server=" `isPrefixOf` a
-                  = makeBot n c (drop 7 a) as
-          makeBot n c s [] = do
-            t <- getClockTime
-            h <- connectTo s (PortNumber (fromIntegral port))
-            hSetBuffering h NoBuffering
-            return Bot { startTime=t 
-                       , socket=h 
-                       , nickname=n 
-                       , channel=c 
-                       , server=s }
-          notify = bracket_
-                   (print "Connecting to server ... " >> hFlush stdout)
-                   (putStrLn "done.")
+type ActionList = [(String, Request -> IrcState Action)]
 
--- We're in the Net monad now, so we've connect successfully
--- Join a channel, and start processing commands
-run :: Net ()
-run = do
-  name <- asks nickname
-  write $ IrcC.nick name
-  write $ IrcC.user (take 6 name ++ "-bot") "0" "*" "some kinda robot"
-  asks socket >>= listen
+data Action = Join Channel
+            | Part Channel
+            | Nick UserName
+            | Echo Channel String
+            | Quit String
+            | Pong String
+            | Idle
 
-----------------------------
------------- IO ------------
-----------------------------
+data Request = UserReq String Channel String
+             | Ping String
+             | Welcome
+             | None
 
-io :: IO a -> Net a
+io :: IO a -> IrcState a
 io = liftIO
 
--- prints message to stdout & network
-write :: IrcB.Message -> Net ()
-write msg = do
-  h <- asks socket
-  io $ hPutStrLn h str
-  io $ putStrLn str
-    where str = IrcB.encode msg
+main :: IO ()
+main = bracket connect disconnect loop
+    where
+      disconnect = hClose . handle
+      loop       = runReaderT run
+
+-- initialize connection
+connect :: IO IrcData
+connect = do
+  handle <- connectTo defaultServ defaultPort
+  hSetBuffering handle NoBuffering
+  return $ IrcData handle defaultName defaultActs
+
+-- inform server of identity, then listen
+run :: IrcState ()
+run = do
+  name <- asks userName
+  write $ nick name
+  write $ user (take 6 name ++ "-bot") "0" "*" "GOLEM XIV"
+  asks handle >>= listen
 
 -- waits for message, decodes & passes to parser, waits again
-listen :: Handle -> Net ()
+listen :: Handle -> IrcState ()
 listen h = forever $ do
-       msg <- IrcP.decode <$> io (hGetLine h)
-       case msg of 
-         Nothing -> return ()
-         Just m -> io (putStrLn $ IrcB.encode m) >> parse m
+  msg <- decode <$> io (hGetLine h)
+  case msg of
+    Nothing -> return ()
+    Just m -> do 
+             io (putStrLn $ encode m)
+             readMsg m >>= consider >>= respond
 
--- determines what to do with message based on command field
-parse :: IrcB.Message -> Net ()
-parse (IrcB.Message pre cmd params) =
-    case cmd of
-      "PING" -> write $ IrcB.Message pre "PONG" params
-      "001" -> asks channel >>= joinChan
-      "PRIVMSG" -> readMsg pre params
-      _ -> return ()
+-- searches message for potential request
+readMsg :: Message -> IrcState Request
+readMsg (Message prefix command params) =
+    case command of
+      "PRIVMSG" -> readPriv prefix (head params) (head $ tail params)
+      "PING"    -> return $ Ping $ unwords params
+      "001"     -> return Welcome
+      _         -> return None
 
--- checks if incoming private messages start with ! or botname
-readMsg :: Maybe IrcB.Prefix -> [IrcB.Parameter] -> Net ()
-readMsg (Just nick) [chan,text] =
-        let (first:rest) = words text in
-        do botName <- asks nickname
-           case first of
-             ('!':cmd) -> eval nick (cmd:rest) chan
-             botName -> eval nick rest chan
+-- searches private message for user request
+readPriv :: Maybe Prefix -> Channel -> String -> IrcState Request
+readPriv (Just (NickName name _ _)) chan stmt = do
+  botName <- asks userName
+  return
+    (if botName `isPrefixOf` stmt then
+      UserReq name chan $ drop (1+length botName) stmt
+    else if "!" `isPrefixOf` stmt then
+      UserReq name chan $ drop 1 stmt
+    else None)
 
--- evaluates messages that appear to be intended for bot
-eval :: IrcB.Prefix -> [IrcB.Parameter] -> IrcC.Channel -> Net ()
-eval nick (cmd:args) chan =
-    case lookup cmd actions of
-      Nothing -> return ()
-      Just action -> action nick args chan
-eval _ _ _ = return ()
+-- determines appropriate action for given request
+consider :: Request -> IrcState Action
+consider None = return Idle
+consider (Ping p) = return $ Pong p
+consider Welcome = return $ Join defaultChan
+consider (UserReq name chan txt) = 
+    let actionWord = takeWhile (not . isSpace) txt
+        actionArgs = drop (1+length actionWord) txt in do
+   actionList <- asks actions
+   case lookup actionWord actionList of
+     Nothing -> return Idle
+     Just action -> action $ UserReq name chan actionArgs
 
----------------------------
---------- Actions ---------
----------------------------
+-- carries out some action
+respond :: Action -> IrcState ()
+respond (Echo chan text) = write $ privmsg chan text
+respond (Pong text) = write $ Message Nothing "PONG" [text]
+respond (Join chan) = write $ joinChan chan
+respond (Part chan) = write $ part chan
+respond (Nick name) = do
+                      IrcData h _ a <- ask
+                      local (return $ IrcData h name a) $ listen h
+                      write $ nick name
+respond (Quit msg) | null msg  = do
+                                 write $ quit Nothing
+                                 io exitSuccess
+                   | otherwise = do
+                                 write $ quit $ Just msg
+                                 io exitSuccess
+respond Idle = return ()
 
--- Actions table
-actions :: [(String, IrcB.Prefix -> [IrcB.Parameter] -> IrcC.Channel -> Net ())]
-actions = [ ("quit", quit)
-          , ("uptime", uptime)
-          , ("echo", echo)
-          , ("swap", swap)
-          , ("join", \_ c _ -> joinChan $ unwords c)
-          , ("part", \_ c _ -> partChan $ unwords c)
-          ]
+-- prints message to stdout & network
+write :: Message -> IrcState ()
+write msg = do
+  h <- asks handle
+  io $ hPutStrLn h s
+  io $ putStrLn s
+    where s = encode msg
 
--- changes nickname
-swap :: IrcB.Prefix -> [IrcB.UserName] -> IrcC.Channel -> Net ()
-swap _ [name] _  = do
-  write $ IrcC.nick name
-  Bot t h _ c s <- ask
-  local (return $ Bot t h name c s) $ listen h
+-- change nickname
+nickAct :: Request -> IrcState Action
+nickAct (UserReq _ _ name) | null name = return Idle
+                           | otherwise = return $ Nick name
 
 -- Exit network
-quit :: IrcB.Prefix ->[IrcB.Parameter] -> IrcC.Channel ->  Net ()
-quit _ _ notice = do
-  write $ IrcC.quit $ (\n -> if null n then Nothing else Just n) notice
-  io exitSuccess
+quitAct :: Request -> IrcState Action
+quitAct (UserReq _ _ notice) = return $ Quit notice
 
 -- Echo text
-echo :: IrcB.Prefix -> [IrcB.Parameter] -> IrcC.Channel -> Net ()
-echo n = privmsg n . unwords
+echoAct :: Request -> IrcState Action
+echoAct (UserReq _ c s) = return $ Echo c s
 
--- sends private message
-privmsg :: IrcB.Prefix -> String -> IrcC.Channel -> Net ()
-privmsg (IrcB.NickName n _ _) s c = do 
-  botName <- asks nickname
-  let c' = if c==botName then n else c
-  write $ IrcC.privmsg c' s
 
 -- Join a channel
-joinChan :: IrcB.Parameter -> Net ()
-joinChan chan | null chan = return ()
-              | otherwise = write $ IrcC.joinChan chan
+joinAct :: Request -> IrcState Action
+joinAct (UserReq _ _ chan) | null chan = return Idle
+                           | otherwise = return $ Join chan
 
 -- parts from channel
-partChan :: IrcB.Parameter -> Net ()
-partChan chan | null chan = return ()
-              | otherwise = write $ IrcC.part chan
-
--- Output uptime
-uptime :: IrcB.Prefix -> [IrcB.Parameter] -> IrcC.Channel -> Net ()
-uptime n _ c = do
-         now <- io getClockTime
-         zero <- asks startTime
-         privmsg n c $ pretty $ diffClockTimes now zero
-
-
--- Pretty print the date in '56d 7h 8m 47s' format
-pretty :: TimeDiff -> String
-pretty td = join . intersperse " " . filter (not . null) . map f $
-    [(years          ,"y") ,(months `mod` 12,"m")
-    ,(days   `mod` 28,"d") ,(hours  `mod` 24,"h")
-    ,(mins   `mod` 60,"m") ,(secs   `mod` 60,"s")]
-  where
-    secs    = abs $ tdSec td  ; mins   = secs   `div` 60
-    hours   = mins   `div` 60 ; days   = hours  `div` 24
-    months  = days   `div` 28 ; years  = months `div` 12
-    f (i,s) | i == 0    = []
-            | otherwise = show i ++ s
-
+partAct :: Request -> IrcState Action
+partAct (UserReq _ _ chan) | null chan = return Idle
+                           | otherwise = return $ Part chan
